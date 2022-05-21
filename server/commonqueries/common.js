ac1142a3feaf3f222cbc08e77e6f2e3ef6095290
divyabalan123 <93704059+divyabalan123@users.noreply.github.com>
2022-05-21 10:44:18 +0530

    common queries

---
+++ server/commonqueries/common.js
@@ -0,0 +1,42 @@
+class APIFeatures{
+    constructor(query,queryString){
+        this.query = query
+        this.queryString = queryString
+    }
+
+    sort(){
+        if(this.queryString.sort){
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            this.query = this.query.sort(sortBy)
+        }
+        else{
+            this.query = this.query.sort('-createdAt')
+        }
+        return this
+        }
+
+    paginate(){
+        const page = this.queryString.page * 1||1
+        const limit = this.queryString.limit *1||100
+        const skip = (page-1) * limit
+        this.query = this.query.skip(skip).limit(limit)
+
+        return this
+    }
+ 
+}
+
+
+const wrapper = (fn)=>{
+    return async (req,res,next)=>{
+        try{
+            await fn(req,res,next)
+            
+        }
+        catch(error){
+            next(error)
+        }
+    }
+}
+
+module.exports = {APIFeatures,wrapper}
--- server/commonqueries/studentt.js
+++ server/commonqueries/studentt.js
@@ -1,8 +1,13 @@
+
+
+
+
+
 const req = require("express/lib/request")
 
 const res = require('express/lib/response')
 
-var userdb = require('../model/model')
+var students = require('../model/student-model')
 
 const {APIFeatures} = require ('../commonqueries/common')
 
@@ -13,37 +18,35 @@ const findone = async (value,req,res)=>{
     
     {
         
-        var features= new APIFeatures(userdb.find(value),req.query).sort().paginate()
+        var features= new APIFeatures(students.find(value),req.query).sort().paginate()
         var data = await features.query
        
         if(!data){
             res.status(202).json({message:'no content '})
         }
         else{
-            console.log('error')
-            res.status(200).json({
+           res.status(200).json({
             
-            message:"found the details of teachers",
+            message:" Success!!found the details of students",
             list:data
         })
         
         }
     }
     catch(err){
-        
-        res.status(500).json({message:err.messsage||'not found the value'})
+      return  res.status(500).json({message:err.messsage||'not found the value'})
     }
 
   }
   
    const findandupdate =async (query,req,res)=>{
-   return await userdb.findByIdAndUpdate(query,req.body,{useFindAndModify:false})
+   return await students.findOneAndUpdate(query,req.body,{useFindAndModify:false})
     .then(data=>{
         if(!data){
             res.status(202).json({message:`no content with ${id}`})
         }else{
             res.status(200).json({
-                message:"updated the details of teacher",
+                message:"updated the details of Students and listed",
                 list: data
             })
         }
@@ -59,12 +62,14 @@ const findone = async (value,req,res)=>{
 
 const deleted = async (query,res)=>{
 
-  return  await userdb.findByIdAndDelete(query) .then(data=>{
+  return  await students.findByIdAndDelete(query) .then(data=>{
       if(!data){
           res.status(202).send(`no content with ${query}`)
       }
       else{
-      res.status(200).send('deleted')
+      res.status(200).json({
+          message:`successfully!! deleted the details of ${query}`
+      })
       }
   })
   .catch(err=>{
